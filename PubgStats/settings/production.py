e38b7b144fd0367cc053f372a24a1142789c3e60
GAURAVCHIT <gaurav198.rr@gmail.com>
2021-01-12 02:22:28 +0530

    heroku: local database added

--- PubgStats/settings/production.py
+++ PubgStats/settings/production.py
@@ -4,6 +4,17 @@ import django_heroku
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'PUBG_DEV_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
 DEBUG = False
 
 ALLOWED_HOSTS = ['esportslover.herokuapp.com']
